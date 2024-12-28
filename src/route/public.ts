1615be7e9412b2dce624439e15fc84a7c063c5a5
Ueno <153161447+Uenoooooo@users.noreply.github.com>
2024-12-28 17:25:19 +0700

    Delete src directory

--- src/route/public.ts
+++
@@ -1,8 +0,0 @@
-// this router can only be accessed by unauthenticated people
-
-import express from "express"
-import { UserController } from "../controller/usercontroller"
-
-export const publicRouter = express.Router()
-publicRouter.post("/api/register", UserController.register)
-publicRouter.post("/api/login", UserController.login)
--- src/validation/validation.ts
+++
@@ -1,8 +0,0 @@
-import { ZodType } from "zod"
-
-export class Validation {
-	static validate<T>(schema: ZodType, data: T): T {
-		// konversi data otomatis menjadi tipe data yang diharapkan
-		return schema.parse(data)
-	}
-}
